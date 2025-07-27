b20d0ea50e1fd0fdfbdaa650c64fc5df436e3886
B Lundin <b.f.lundin@gmail.com>
2025-07-27 22:09:29 +0200

    semi workin. removed template c-code

--- pipes/pipes.adb
+++ pipes/pipes.adb
@@ -1,57 +1,9 @@
-size_t tally = 0;
-
-void* write_loop(void *a)
-{
-    int fd;
-    char buf[32];
-    while (1) {
-        /* block open, until a reader comes along */
-        fd = open("out", O_WRONLY);
-        write(fd, buf, snprintf(buf, 32, "%d\n", tally));
-        close(fd);
-
-        /* Give the reader a chance to go away. We yeild, OS signals
-           reader end of input, reader leaves. If a new reader comes
-           along while we sleep, it will block wait. */
-        usleep(10000);
-    }
-}
-
-void read_loop()
-{
-    int fd;
-    size_t len;
-    char buf[PIPE_BUF];
-    while (1) {
-        fd = open("in", O_RDONLY);
-        while ((len = read(fd, buf, PIPE_BUF)) > 0)
-          tally += len;
-        close(fd);
-    }
-}
-
-int main()
-{
-    pthread_t pid;
-
-    /* haphazardly create the fifos.  It's ok if the fifos already exist,
-       but things won't work out if the files exist but are not fifos;
-       if we don't have write permission; if we are on NFS; etc.  Just
-       pretend it works. */
-    mkfifo("in", 0666);
-    mkfifo("out", 0666);
-
-    /* because of blocking on open O_WRONLY, can't select */
-    pthread_create(&pid, 0, write_loop, 0);
-    read_loop();
-
-    return 0;
-}
-
 
 
 with interfaces.c;
 use interfaces;
+
+with text_io;
 
 procedure Pipes is
 
@@ -64,25 +16,29 @@ procedure Pipes is
 
   Global_tally : Ssize_T := 0;
   
+  O_RDWR : aliased c.int := 0;
+--  pragma import(C, O_RDWR, "O_RDWR");
+
+  O_WRONLY : aliased c.int := 1;
+ -- pragma import(C, O_WRONLY, "O_WRONLY");
+
   
   use type C.Int;
   
-   procedure create_fifo(name : string) is
-   
-    Result : C.Int := -1;
-   
+  procedure create_fifo(name : string) is
+    Result : C.Int := -1;   
     subtype Path_Name_Type is String(name'first..name'last +1);
     My_Path : aliased Path_Name_Type := Name & Ascii.Nul;
+
     function Mkfifo(Path : access Path_Name_Type; Permission : Mode_t) return C.Int;
     pragma Import(C, mkfifo, "mkfifo");
-
    begin
-     Result := Mkfifo(My_Path'access, 8#666# );
+     Result := Mkfifo(My_Path'access, 8#660# );
      pragma Assert(Result >= 0, "bad mkfifo:" & Result'Image);
    end create_fifo;
    -------------------------------
    
-  function  Open(name : string; Flags : Interfaces.C.Int) return File_id is
+  function Open(name : string; Flags : Interfaces.C.Int) return File_id is
     subtype Path_Name_Type is String(name'first..name'last +1);
     My_Path : aliased Path_Name_Type := Name & Ascii.Nul;
 
@@ -90,12 +46,12 @@ procedure Pipes is
                    Flags : Interfaces.C.Int;
                    Mode  : Mode_T ) return File_Id;
     pragma Import(C, cOpen, "open" );
-    Result : C.Int := -1;
+    Result : file_id := -1;
 
   begin
     Result := cOpen(My_Path'access,Flags, 8#660#);
     pragma Assert(Result >= 0, "bad Open:" & Result'Image);
-    return File_id(Result);
+    return Result;
   end Open;
       
   -----------------------------------
@@ -118,10 +74,10 @@ procedure Pipes is
   begin
     Len := cRead(File, Message_Buffer'access, Size_T(message_type'last));
     pragma Assert(Len >= 0, "bad Read:" & Len'image);
+    Msg := Message_Buffer;
   end Read;
     
   ------------------------------
-  
 
   procedure Write(File : File_Id; Msg : in string) is
   
@@ -135,42 +91,50 @@ procedure Pipes is
     dummy : Ssize_T;
   begin
     dummy := cWrite(File, My_Value'access, Size_T(My_Value'last));
-    pragma Assert(dummy >= 0, "bad Write:" & Len'image);
+    pragma Assert(dummy >= 0, "bad Write:" & dummy'image);
   end Write;
-  
-  
+    
   -------------------------------
+
   procedure read_loop is
-    fd : interfaces.c.int;
+    fd : file_id;
     buf : message_type;
     len : Ssize_T;
-    dummy : c.int  :
+    dummy : c.int  ;
   begin
     outer : loop
+      text_io.put_line ("read_loop-outer: before open");
       fd := open("in_pipe", O_RDWR);
       inner : loop
+        text_io.put_line ("read_loop-inner: before read");
         read(fd, buf, len);
+        text_io.put_line ("read_loop-inner: read" & len'img & " bytes '" & buf(1..integer(len)) & "'");
         exit inner when len = 0;
         Global_tally := Global_tally + len;
       end loop inner;
-      
+      text_io.put_line ("read_loop-outer:  before close");      
       dummy := close(fd);
     end loop outer;
   end read_loop;
   ------------------------------
   
   task writer is
-    accept start;
+    entry start;
   end writer;
 
   task body writer is
     fd : file_id;
     dummy : c.int;
   begin
+    text_io.put_line ("task writer: wait for start");      
+
     accept start; -- wait here until called. Rendez-vouz is done here
+    text_io.put_line ("task writer: start called");      
     
     loop
+      text_io.put_line ("task writer-loop: wait for open call by others");      
       fd := open("out_pipe", O_WRONLY);  -- block open, until a reader comes along
+      text_io.put_line ("task writer: wrote" & Global_tally'image);      
       Write(fd,Global_tally'image);
       dummy := Close(fd);
       delay 0.0; -- force context change
